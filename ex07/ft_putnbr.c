664fcd3d3c6071d2bc91bbcac0a81c713afc8185
Sus <sus@example.com>
2026-03-10 13:43:30 +0300

    C00 ex07

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
